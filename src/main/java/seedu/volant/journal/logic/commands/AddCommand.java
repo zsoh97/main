3752c82670b9388e8e99fe715285004d1b259f17
Mikwin <mikhailfaiz@hotmail.com>
2020-03-25 02:25:51 +0800

    Updated portfolio and example add entry

--- src/main/java/seedu/volant/journal/logic/commands/AddCommand.java
+++ src/main/java/seedu/volant/journal/logic/commands/AddCommand.java
@@ -32,10 +32,10 @@ public class AddCommand extends Command {
             + PREFIX_WEATHER + "WEATHER \n"
 
             + "Example: " + COMMAND_WORD + " "
-            + PREFIX_DATE + "11-09-2001"
+            + PREFIX_DATE + "28-01-2020 "
             + PREFIX_TIME + "04:20 "
-            + PREFIX_TEXT + "I love strawberries "
-            + PREFIX_LOCATION + "World Peace Centre "
+            + PREFIX_TEXT + "I have coronavirus and my wife has left me "
+            + PREFIX_LOCATION + "Wuhan "
             + PREFIX_FEELING + "SCARED "
             + PREFIX_WEATHER + "COOL ";
 
