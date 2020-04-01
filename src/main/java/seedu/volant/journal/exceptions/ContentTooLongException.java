de1d377ea3b1083d8335edf50486bd06ebe5017b
Mikwin <mikhailfaiz@hotmail.com>
2020-04-02 00:45:33 +0800

    Codacy fix

--- src/main/java/seedu/volant/journal/exceptions/ContentTooLongException.java
+++ src/main/java/seedu/volant/journal/exceptions/ContentTooLongException.java
@@ -6,7 +6,6 @@ import seedu.volant.commons.logic.parser.exceptions.ParseException;
  * Represents error when content of a journal entry is greater than 280 characters.
  */
 public class ContentTooLongException extends ParseException {
-    private int contentOverLimit;
     public ContentTooLongException(int contentOverLimit) {
         super("I'm sorry, journal entry content has to be 280 characters or less :( \n"
                 + "You are currently " + contentOverLimit + " characters over the limit.");
--- src/main/java/seedu/volant/journal/model/entry/UniqueEntryList.java
+++ src/main/java/seedu/volant/journal/model/entry/UniqueEntryList.java
@@ -31,9 +31,6 @@ public class UniqueEntryList implements Iterable<Entry> {
     private final ObservableList<Entry> internalUnmodifiableList =
             FXCollections.unmodifiableObservableList(internalList);
 
-    /* TODO: This class is adapted from UniqueTripList.
-     * Implement all these methods once implementation of Journal is complete.
-     */
 
     /**
      * Returns true if the list contains an equivalent trip as the given argument.
